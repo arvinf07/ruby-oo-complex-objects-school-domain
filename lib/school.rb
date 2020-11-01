6238bb92d28044dcb076390a74f9bfb990fd3f13
Arvin Fernandez <arvinf07@gmail.com>
2020-11-01 14:41:09 -0500

    commit tree ba23f84533a20f9d5bda1f4ebc04922b2b941065

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(school)
+    @name = school
+    @roster = {}
+  end  
+
+  def add_student(student, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade] << student  
+    end  
+  end
+  
+  def grade(grade)
+    if @roster.keys.include?(grade)
+      @roster[grade]
+    else
+      puts "This grade does not exist"  
+    end  
+  end  
+
+  def sort
+    @roster.collect {|grade, students|  @roster[grade] = students.sort}
+    @roster
+  end  
+  
+end
